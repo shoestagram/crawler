4d4b4e0b236d3141ad9677725bf2ce52e3bfdf5c
Melinda Yeoh OG <melindayeoh OG@melinda-yeohs-macbook.local>
2017-02-20 15:59:18 -0500

    Cleaned up tumblr.js

--- tumblr.js
+++ tumblr.js
@@ -43,57 +43,6 @@ blog.photo({limit: 2}, function(error, response) {
   });
 
   console.log(ccPhotos);
-  // console.log(ccPhotos[0].thumbnail_url);
-  //console.log(response.posts);
-  //MAKING A PHOTO FROM FIRST BLOG POST
-  // var date_created = response.posts[0].date;
-  // var media_url = response.posts[0].post_url;
-  // var text = response.posts[0].summary;
-  // var keyword = response.posts[0].tags;
-  // var source_id = response.posts[0].id;
-  // var source_url = response.posts[0].post_url;
-  // var source_user = response.posts[0].blog_name;
-
-
-  // var shoppingURL = response.posts[0].link_url; //sometimes blog post won't have this
-
-  // if (shoppingURL){
-  //   crawled_shops_links === true;
-  // }
-
-
-  // var photos = response.posts[0].photos[0].alt_sizes;
-  // //console.log("photos: ", photos);
-
-  // //loop through the photos of first blog, and find the photo with a width ===400
-  // photos.forEach(function(photo){
-  // 	if(photo.width === 400){
-  // 		// width = photo.width;
-  // 		// height= photo.height;
-  // 		thumbnail_url = photo.url;
-  // 	}
-
-  //   //console.log("photo width: ", photo.width, typeof photo.width); //outputs number
-  //   //console.log("photo height: ", photo.height, typeof photo.height); //number too
-  // });
-
-  // // console.log("width: ", width);
-  // // console.log("height: ", height);
-  // // console.log("url: ", imageURL);
-  // // console.log("shopping URL: ", response.posts[1].link_url);
-
-  // var firstPhoto = {
-  // 	dateCreated: dateCreated,
-  // 	postURL: postURL,
-  // 	text: text,
-  // 	keywords: tags,
-  // 	imageURL: imageURL,
-  // 	imageWidth: width,
-  // 	imageHeight: height,
-  // 	shoppingURL: shoppingURL
-  // }
-
-  // console.log("first photo: ", firstPhoto);
 
 
 });
